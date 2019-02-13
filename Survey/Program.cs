7e5c06bdeaf10f36f2f35f951ac3f7f2c56fe65c
Arun Kumar Singh <arunpratap26@yahoo.com>
2019-02-14 05:00:43 +0530

    Styled Console color

--- Survey/Program.cs
+++ Survey/Program.cs
@@ -6,15 +6,25 @@ namespace Survey
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("What is your name?");
+            Console.ForegroundColor = ConsoleColor.White;
             var name = Console.ReadLine();
 
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("What is your age?");
+            Console.ForegroundColor = ConsoleColor.White;
             var age = Console.ReadLine();
 
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("What month were you born in?");
+            Console.ForegroundColor = ConsoleColor.White;
             var month = Console.ReadLine();
 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Your answer ");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Your name is: {0}", name);
             Console.WriteLine("Your age is: {0}", age);
             Console.WriteLine("Your birth month is: {0}", month);
