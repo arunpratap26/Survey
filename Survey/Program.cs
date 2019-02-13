e8a6b329f66c47ef42ec2fa9c15182a8f0476db0
Arun Kumar Singh <arunpratap26@yahoo.com>
2019-02-14 04:12:22 +0530

    first commit

--- Survey/Program.cs
+++ Survey/Program.cs
@@ -6,7 +6,32 @@ namespace Survey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine("Your age is: {0}", age);
+            Console.WriteLine("Your birth month is: {0}", month);
+
+            if (month == "march")
+            {
+                Console.WriteLine("you are an Aries.");
+            }
+            else if (month == "april")
+            {
+                Console.WriteLine("you are a Taurus.");
+            }
+            else if (month == "may")
+            {
+                Console.WriteLine("you are a Gemini.");
+            }
+
         }
     }
 }
